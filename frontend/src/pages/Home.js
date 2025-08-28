ee41a0f84d9d3e42503d5808f152e92737ccd02d
YM0919 <yongyao0919@gmail.com>
2025-08-28 06:39:33 +0100

    adjust to make reponsive

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -15,8 +15,8 @@ function Home() {
         {label1: "GACTGCAATACTCGCCAGCC", label2: "(Spacer sequence (raw))", field: "spacer_sequence_raw", query: "GACTGCAATACTCGCCAGCC"},
         {label1: "NNNNNNNAAAAAAAAACTCCAAAACCCTGGNNNNNNNNNNNN", label2: "(Target context sequence)", field: "target_context_sequence", query: "NNNNNNNAAAAAAAAACTCCAAAACCCTGGNNNNNNNNNNNN"},
         {label1: "Sniper-Cas9-NLS-FLAG-P2A", label2: "(Variant)", field: "variant", query: "Sniper-Cas9-NLS-FLAG-P2A"},
-        {label1: "SpCas9", label2:"(Nuclease)", field: " nuclease", query: "SpCas9"},
-        {label1: "SpCas9 scaffold 1", label2: "(gRNA scaffold)", field: "gRNA_scaffoldd", query: "SpCas9 scaffold 1"},
+        {label1: "SpCas9", label2:"(Nuclease)", field: "nuclease", query: "SpCas9"},
+        {label1: "SpCas9 scaffold 1", label2: "(gRNA scaffold)", field: "gRNA_scaffold", query: "SpCas9 scaffold 1"},
         {label1: "['Wild SpCas9']", label2: "(Study)", field: "study", query: "['Wild SpCas9']"},
         {label1: "3", label2: "(Number of mismatches)", field: "number_of_mismatches", query: "3"},
         {label1: "Lentiviral", label2: "(Cas12a transfection)", field: "cas12a_transfection", query: "Lentiviral"}
@@ -53,42 +53,43 @@ function Home() {
 
     return (
         <div>
-            <div class="video-background">
+            <div className="video-background">
                 <iframe src="https://player.vimeo.com/video/871987849?autoplay=1&loop=1&muted=1&background=1"
                     title='CRISPR/Cas9 interaction video' 
-                    frameborder="0" 
-                    allow="autoplay; fullscreen" 
-                    allowfullscreen>
+                    frameborder="0"
+                    allowFullscreen>
                 </iframe>
             </div>
-            <div class="main-content">
-                <div class="container">
-                    <div class="content">
-                        <div class="text-column">
-                            <h1>Welcome to the CasVarDB</h1>
-                            <p>
-                                The CasVarDB contains information on guide RNA sequences, target sites, experimental conditions, and background-subtracted indel frequencies for both Cas9 and Cas12 systems.&nbsp;
-                                The <strong>Cas9 dataset</strong> spans <strong><span class="standout-text">7</span></strong> studies with a total of <strong><span class="standout-text">1,746,674</span></strong> datapoints,&nbsp;
-                                covering <strong><span class="standout-text">40</span></strong> Cas9 variants and <strong><span class="standout-text">16</span></strong> gRNA scaffolds.&nbsp;
-                                The <strong>Cas12 dataset</strong> includes <strong><span class="standout-text">250,254</span></strong> datapoints from <strong><span className="standout-text">4</span></strong> studies,&nbsp;
-                                featuring <strong><span className="standout-text">26</span></strong> Cas12 variants and <strong><span className="standout-text">6</span></strong> gRNA scaffolds.&nbsp;
-                                These datasets support research on CRISPR activity, specificity, and editing efficiency, serving as a valuable resource for deep learning models and bioinformatics analysis.
+            <div className="main-content">
+                <div className="container">
+                    <div className="content">
+                        <div className="text-column">
+                            <div className='text-box'>
+                                <h1>Welcome to the CasVarDB</h1>
+                                <p>
+                                    The CasVarDB contains information on guide RNA sequences, target sites, experimental conditions, and background-subtracted indel frequencies for both Cas9 and Cas12 systems.&nbsp;
+                                    The <strong>Cas9 dataset</strong> spans <strong><span className="standout-text">7</span></strong> studies with a total of <strong><span className="standout-text">1,746,674</span></strong> datapoints,&nbsp;
+                                    covering <strong><span className="standout-text">40</span></strong> Cas9 variants and <strong><span className="standout-text">16</span></strong> gRNA scaffolds.&nbsp;
+                                    The <strong>Cas12 dataset</strong> includes <strong><span className="standout-text">250,254</span></strong> datapoints from <strong><span className="standout-text">4</span></strong> studies,&nbsp;
+                                    featuring <strong><span className="standout-text">26</span></strong> Cas12 variants and <strong><span className="standout-text">6</span></strong> gRNA scaffolds.&nbsp;
+                                    These datasets support research on CRISPR activity, specificity, and editing efficiency, serving as a valuable resource for deep learning models and bioinformatics analysis.
                                 </p>
-                            
-                            <div class="text-column-buttons">
-                                <a href='/statistics'>
-                                    <button type="button">Statistics  <i class="bi bi-bar-chart-line-fill"></i></button>
-                                </a>
-                                <a href='#'>
-                                    <button type="button" onClick={() => setShow(true)}>Download  <i class="bi bi-download"></i></button>
-                                </a>
+                                
+                                <div className="text-column-buttons">
+                                    <a href='/statistics'>
+                                        <button type="button">Statistics  <i className="bi bi-bar-chart-line-fill"></i></button>
+                                    </a>
+                                    <a href='#'>
+                                        <button type="button" onClick={() => setShow(true)}>Download  <i className="bi bi-download"></i></button>
+                                    </a>
+                                </div>
                             </div>
                         </div>
                 
-                        <div class="search-column">
-                            <div class="search-box">
-                                <div class="search-box-top">
-                                    <h2><i class="bi bi-search"></i>  Search Database</h2>
+                        <div className="search-column">
+                            <div className="search-box">
+                                <div className="search-box-top">
+                                    <h2><i className="bi bi-search"></i>  Search Database</h2>
                         
                                     <select value={searchField} onChange={(e) => setSearchField(e.target.value)}>
                                         <option value="spacer_sequence_raw">Spacer sequence (raw)</option>
@@ -117,7 +118,7 @@ function Home() {
                                     <button className="dropbtn" 
                                         onClick={() => { if (selectedDB) handleSearch(selectedDB);}}
                                     > 
-                                    {selectedDB === null ? 'SEARCH' : `Search ${selectedDB}`} <i class="bi bi-caret-down-fill"></i>
+                                    {selectedDB === null ? 'SEARCH' : `Search ${selectedDB}`} <i className="bi bi-caret-down-fill"></i>
                                     </button>
                                     <div className="dropdown-content">
                                         <a href='#' 
