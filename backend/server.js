5379040983872bcfc903b1e4f744958daf2e4f06
Y-0919 <yongyao0919@gmail.com>
2025-09-18 21:29:04 +1000

    modify port

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-// const port = 8888;
-const port = 5000;
+const port = 8888;
+// const port = 5000;
 
 
 app.use(cors());
